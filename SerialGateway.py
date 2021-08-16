077e32125b5de41aaee840ce0ef1bdcbb857cb10
Stephen <stevogray@gmail.com>
2021-08-16 21:31:04 +0930

    Encode Serial writes, strip newlines, + MQTT codes

--- SerialGateway.py
+++ SerialGateway.py
@@ -3,6 +3,9 @@
 # Python script to convert serial traffic to MQTT and vice versa.
 # Stephen Gray 2017/04/15
 # David deMarco 2017/07/9 - Updated to Python 3.5 and added logging
+# SG 2019/06/15 Encode serial writes
+# SG 2019/07/03 Strip newlines from topic and payload
+# SG 2021/08/16 Add MQTT connection error codes. Change logging levels
 #
 # Serial data arrives as comma delimited in the following form (comma delimited, colons separate MQTT topic and message):
 # {sending_node_number},{topic}:{message},{topic}:{message}
@@ -18,7 +21,7 @@
 # sensors/>/{nodeid}/{topic} {message}
 # Any new data is sent to serial as:
 # <{nodeid}:{topic}:{message}>
-# eg Topic: sensors/>/5/LED Message:ON or Topic: sensors/>/5 Message:LED:ON would be sent to serial as:
+# eg Topic: sensors/>/5/LED Message:ON would be sent to serial as:
 # <5:LED:ON>
 # Which the attached arduino broadcasts over the wireless network. The code in the arduino should look for the start marker "<" and the end marker ">".
 # Any message destined for node 1 (the gateway ardunio) is written to serial without sending node.
@@ -42,12 +45,8 @@ import logging.handlers
 
 
 ##############Logging Settings##############
-#Added a logging routine so when the gateway is run as a process on the PI you can see what is going on.  Main change you
-#you might want to make is the logging level, in the settings file.
-
-# Change the above to .DEBUG for message infomation and .INFO for runtime minimal messages
-logging.basicConfig(level=logging.DEBUG)
-LOGGER = logging.getLogger(__name__)
+#Added a logging routine so when the gateway is run as a process on the PI you can see what is going on.
+#Set the logging level in the settings file.
 
 # Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
 # Give the logger a unique name (good practice)
@@ -64,12 +63,28 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 #########################################
 
-logger.critical('Starting up Pi/Moteino Gateway')
+logger.critical('Starting Pi/Moteino Gateway')
+logger.info("Log level: "+ s.LOG_LEVEL)
+
+def error(code):
+    errorlist = {
+        -1: "Socket Error",
+        0: "Successful Connect",
+        1: "Unsupported MQTT version",
+        2: "Client ID rejected",
+        3: "Server unavailable",
+        4: "Invalid username/password",
+        5: "Not authorized",
+        }
+    return errorlist.get(code, "Unknown error")
 
 #MQTT callbacks
 #the callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    logger.error("Connected to MQTT broker with result code "+str(rc))
+    if rc == 0: 
+        logger.info("Connected to MQTT broker with result: "+ error(rc))
+    else:
+        logger.error("Connected to MQTT broker with result: "+ error(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("sensors/>/#")
@@ -80,21 +95,22 @@ def on_message(client, userdata, msg):
 	#If node number is 1, pass through msg.payload
     #If topic has a fourth level, add this to the serial string
     list1=msg.topic.split("/")
+    msg.payload = msg.payload.decode("utf-8")
+
     if len(list1)==3 and list1[2]=="1":
         sendstr=("<"+msg.payload+">")
-        ser.write(sendstr)
+        ser.write(sendstr.encode())
         logger.info("Send serial to gateway: "+ sendstr)
     elif len(list1)==3:
         sendstr=("<"+list1[2]+":"+msg.payload+">")
         logger.info("Send serial: "+ sendstr)
-        ser.write(sendstr)
+        ser.write(sendstr.encode())
     elif len(list1)==4:
         sendstr=("<"+list1[2]+":"+list1[3]+":"+msg.payload+">")
         logger.info("Send serial: "+ sendstr)
-        ser.write(sendstr)
+        ser.write(sendstr.encode())
 
 def on_publish(client, userdata, mid):
-#    print "Published ", mid
     pass
 
 #called on program exit
@@ -145,17 +161,20 @@ try:
         #Format of serial line is:
         #<sending_node_number>,<topic>:<payload>,<topic>:<payload>, etc, <RSSI>:<value>,ACK:<sent or not>
         try:
-            nodenum=items[0]
-            #crash out of the try if the first item is not a number
-            int(nodenum)
-            logger.debug("From node number: %s", nodenum)
-            for item in items[1:]:
-                if ":" in item:
-                    data=item.split(":")
-                    #first element is topic, second is payload
-                    topic="sensors/</"+nodenum+"/"+data[0]
-                    mqttc.publish(topic, data[1])
-                    logger.info('topic: %s Data: %s ', topic, data[1])
+            if len(items)==1:
+                logger.warning(serialreadline)
+            else:
+                nodenum=items[0]
+                #crash out of the try if the first item is not a number
+                int(nodenum)
+                logger.debug("From node number: %s", nodenum)
+                for item in items[1:]:
+                    if ":" in item:
+                        data=item.split(":")
+                        #first element is topic, second is payload
+                        topic="sensors/</"+nodenum+"/"+data[0].strip()
+                        mqttc.publish(topic, data[1].strip())
+                        logger.info("Received data: topic: %s Data: %s ", topic, data[1].strip())
         except Exception as e:
             logger.warning('Error parsing data %s',format(str(e)))
             pass
